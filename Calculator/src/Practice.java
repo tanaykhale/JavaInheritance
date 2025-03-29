95738ab38c9c076bfb8203b4f324b4dfd4ceec61
Tanay Khale <91783433+tanaykhale@users.noreply.github.com>
2025-03-29 23:10:15 +0530

    new

---
+++ Calculator/src/Practice.java
@@ -0,0 +1,22 @@
+
+interface  A{
+	public static final int a = 10;
+	 void display() ;
+}
+class  B {
+	public void display() {
+		System.out.println("hello");
+	}
+	
+}
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		B a=new B();
+		a.display();
+	}
+
+}
